3fb597dcac2fa000f86efcc551cc2f89f3a332a4
Andrew Scribner <ca.scribner+1@gmail.com>
2020-08-31 10:32:08 -0400

    feat: Add context to train event handler

--- .github/events/event_handler/start_train.py
+++ .github/events/event_handler/start_train.py
@@ -5,15 +5,28 @@ from event_handler.dispatch import BaseDispatchEventHandler
 
 class StartEventHandler(BaseDispatchEventHandler):
 
-    MODEL_START_COMMENT = "Model training has started"
+    MODEL_START_COMMENT = \
+        "Model training has started\n" \
+        "Run ID: {run_id}\n" \
+        "See details at: [link]({run_link})\n" \
+        "Commit sha: {sha}" \
+
     CHECK_RUN_NAME = "Model Training"
     CHECK_RUN_TITLE = "Model Training Pipeline"
     CHECK_RUN_SUMMARY = "In Progress"
     CHECK_RUN_TEXT = "See details at {}"
 
     def dispatch(self):
-        self.add_comment(self.MODEL_START_COMMENT)
+        run_link = os.getenv("KFP_DSHB") + "/_/pipeline/#/runs/details/" + self.run_id
+        message = self.MODEL_START_COMMENT.format(
+            run_id=self.run_id,
+            sha=self.sha,
+            run_link=run_link
+        )
+
+        self.add_comment(message)
+        
         check_run_text = self.CHECK_RUN_TEXT.format(
-            os.getenv("KFP_DSHB")+"/_/pipeline/#/runs")
+            run_link)
         self.create_check_run(
             self.CHECK_RUN_NAME, self.CHECK_RUN_TITLE, self.CHECK_RUN_SUMMARY, check_run_text)
